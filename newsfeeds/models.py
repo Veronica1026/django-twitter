fc2ddda8f92e255c4fcb18a31d0b321cc5f04f09
Veronica <543587590@qq.com>
2021-05-16 08:53:11 +1000

    newsfeeds model

---
+++ newsfeeds/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tweets.models import Tweet
+
+
+class NewsFeed(models.Model):
+    # this user is the person who can view the tweet, not the author of the tweet
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
+    # used for sorting. we can not easily sort this table using the time column from tweet table.
+    # so created this extra column here
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = (('user', 'created_at'),)
+        unique_together = (('user', 'tweet'),)
+        ordering = ('user', '-created_at',)
+
+    def __str__(self):
+        return f'{self.created_at} inbox of {self.user}: {self.tweet}'
